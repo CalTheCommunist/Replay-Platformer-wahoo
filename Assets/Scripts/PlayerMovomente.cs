244081dac418999af5c7ae5bcec2810de7f42af2
CalTheCommunist <Calvindenbraber5@gmail.com>
2026-09-07 13:52:23 +0200

    SHE MOOOOOVES!

--- Assets/Scripts/PlayerMovomente.cs
+++ Assets/Scripts/PlayerMovomente.cs
@@ -6,7 +6,9 @@ public class PlayerMovomente : MonoBehaviour
     public Rigidbody player;
     public float speed;
     public InputActionReference jumping;
+    public InputActionReference move;
 
+    private Vector2 direction;
     private void Jump(InputAction.CallbackContext obj)
     {
         Debug.Log("JUMP BITCH!");
@@ -24,6 +26,10 @@ public class PlayerMovomente : MonoBehaviour
     
     void Update()
     {
-        
+        direction = move.action.ReadValue<Vector2>();
+    }
+    private void FixedUpdate()
+    {
+        player.linearVelocity = new Vector3(direction.x * speed, 0, direction.y * speed);
     }
 }
