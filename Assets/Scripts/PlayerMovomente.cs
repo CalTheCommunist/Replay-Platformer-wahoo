d7abb4768b24ab3947cec6de3d30b0a6e31e4237
CalTheCommunist <Calvindenbraber5@gmail.com>
2026-09-08 13:23:30 +0200

    CAMERA AND FINISHED MOVOMENTE AAAAAAAA

--- Assets/Scripts/PlayerMovomente.cs
+++ Assets/Scripts/PlayerMovomente.cs
@@ -3,12 +3,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovomente : MonoBehaviour
 {
+    public Transform cam;
     public Rigidbody player;
-    public float speed;
     public InputActionReference jumping;
     public InputActionReference move;
 
+    public float turnsmoothtime = 0.4f;
+    public float speed;
     private Vector2 direction;
+    private float turnvelocity = 5f;
+
+    
+
     private void Jump(InputAction.CallbackContext obj)
     {
         Debug.Log("JUMP BITCH!");
@@ -27,9 +33,21 @@ public class PlayerMovomente : MonoBehaviour
     void Update()
     {
         direction = move.action.ReadValue<Vector2>();
+          
+
+
     }
+   
     private void FixedUpdate()
     {
-        player.linearVelocity = new Vector3(direction.x * speed, 0, direction.y * speed);
+        Vector3 camDir = cam.transform.rotation * direction;
+        Vector3 targetDirection = new Vector3(camDir.x, 0, camDir.z);
+        if(direction != Vector2.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDirection), Time.deltaTime * turnvelocity);
+        }
+        
+        player.linearVelocity = targetDirection.normalized * speed;
+
     }
 }
