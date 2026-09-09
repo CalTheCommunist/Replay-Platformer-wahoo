24a6ae0e825b0e6dcea29ce11cb5f3b3e876a2fd
CalTheCommunist <Calvindenbraber5@gmail.com>
2026-09-09 10:52:00 +0200

    JUMP! JUMP! JUMP! JUMP!

--- Assets/Scripts/PlayerMovomente.cs
+++ Assets/Scripts/PlayerMovomente.cs
@@ -5,19 +5,22 @@ public class PlayerMovomente : MonoBehaviour
 {
     public Transform cam;
     public Rigidbody player;
-    public InputActionReference jumping;
-    public InputActionReference move;
 
+    public InputActionReference move;
     public float turnsmoothtime = 0.4f;
     public float speed;
     private Vector2 direction;
     private float turnvelocity = 5f;
 
-    
+    public InputActionReference jumping;
+    public float jumpHeight = 2f;
+    private bool jumpPressed = false;
+    private bool isGrounded = true;
+
 
     private void Jump(InputAction.CallbackContext obj)
     {
-        Debug.Log("JUMP BITCH!");
+        jumpPressed = true;
     }
 
     private void OnEnable()
@@ -40,14 +43,49 @@ public class PlayerMovomente : MonoBehaviour
    
     private void FixedUpdate()
     {
+        if (jumpPressed && isGrounded)
+        {
+            player.linearVelocity = new Vector3(
+                player.linearVelocity.x,
+                Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y),
+                player.linearVelocity.z
+            );
+
+            isGrounded = false;
+            jumpPressed = false;
+        }
+        if(isGrounded == false && jumpPressed == true)
+        {
+            jumpPressed = false;
+        }
+
         Vector3 camDir = cam.transform.rotation * direction;
         Vector3 targetDirection = new Vector3(camDir.x, 0, camDir.z);
         if(direction != Vector2.zero)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDirection), Time.deltaTime * turnvelocity);
         }
-        
-        player.linearVelocity = targetDirection.normalized * speed;
 
+        player.linearVelocity = new Vector3(
+     targetDirection.normalized.x * speed,
+     player.linearVelocity.y,
+     targetDirection.normalized.z * speed
+ );
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            if (contact.normal.y > 0.5f)
+            {
+                isGrounded = true;
+                break;
+            }
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
     }
 }
